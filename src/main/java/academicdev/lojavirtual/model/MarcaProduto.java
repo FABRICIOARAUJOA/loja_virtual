94b4827f0e33870cacecb3432e029138a9772083
Fabricio Araujo da Silva <fabriciodevti@gmail.com>
2023-06-26 10:49:00 -0300

    implementação classes de model

--- src/main/java/academicdev/lojavirtual/model/MarcaProduto.java
+++ src/main/java/academicdev/lojavirtual/model/MarcaProduto.java
@@ -3,6 +3,7 @@ package academicdev.lojavirtual.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,7 +21,8 @@ public class MarcaProduto implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
 	private Long id;
-
+	
+	@Column(nullable = false)
 	private String nomeDesc;
 
 	public Long getId() {
